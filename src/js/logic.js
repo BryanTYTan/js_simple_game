82f743e624765ede058d649fc36f06f2fbe4f007
bryan <90520318+BryanTYTan@users.noreply.github.com>
2025-03-23 15:25:29 +0800

    Adding some base logic to javascript

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,5 +1,18 @@
-function myFunction() {
-    document.getElementById("demo").style.fontSize = "25px";
-    document.getElementById("demo").style.color = "red";
-    document.getElementById("demo").style.backgroundColor = "yellow";
+const valid_choices = ['rock', 'paper', 'scissors', 'shotgun']
+
+function play_game() {
+    let user_val = document.forms["user_info"]["user_choice"].value;
+
+    if (user_val) {
+        console.log(user_val);
+    }
+    else {
+        let result_div = document.getElementById('results');
+        result_div.innerHTML = "Please Enter a non empty input";
+    }
+
+
+
+
+    return false;
 }
