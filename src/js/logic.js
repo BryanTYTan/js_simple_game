85313d63fef53dbb496436d82ff949b8af65e4f6
bryan <90520318+BryanTYTan@users.noreply.github.com>
2025-03-29 17:41:38 +0800

    Write all game logic

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,18 +1,99 @@
-const valid_choices = ['rock', 'paper', 'scissors', 'shotgun']
+const valid_player_choices = ['rock', 'paper', 'scissors', 'shotgun']
+const valid_computer_choices = ['rock', 'paper', 'scissors']
 
 function play_game() {
     let user_val = document.forms["user_info"]["user_choice"].value;
+    let result_div = document.getElementById('results');
 
     if (user_val) {
-        console.log(user_val);
+        let choice = user_val.toLowerCase();
+
+        // Check if it is a valid input
+        if (valid_player_choices.indexOf(choice) > -1) {
+            let computer_choice = obtain_computer_choice();
+
+            // Auto Win
+            if (valid_player_choices.indexOf(choice) == 3) {
+                do_player_win();
+            }
+
+            // True If player won
+            let result = check_who_won(choice, computer_choice);
+            if (result == 0) {
+                do_player_win();
+            } else if (result == 1) {
+                result_div.innerHTML = "Draw! Play again!";
+            } else {
+                do_computer_Win();
+            }
+
+        } else {
+            result_div.innerHTML = "Please enter a valid input";
+        }
+        
     }
     else {
-        let result_div = document.getElementById('results');
         result_div.innerHTML = "Please Enter a non empty input";
     }
 
-
-
-
     return false;
+}
+
+function check_who_won(player_choice, computer_choice) {
+    // 0 = Win
+    // 1 = Draw
+    // 2 = Lost
+    let win = '0';
+
+    switch(player_choice) {
+        case 'rock':
+            if (valid_computer_choices.indexOf(computer_choice) == 0) {
+                win = 1;
+            } else if (valid_computer_choices.indexOf(computer_choice) == 1) {
+                win = 2;
+            }
+            break;
+
+        case 'paper':
+            if (valid_computer_choices.indexOf(computer_choice) == 1) {
+                win = 1;
+            } else if (valid_computer_choices.indexOf(computer_choice) == 2) {
+                win = 2;
+            }
+            break;
+
+        case 'scissors':
+            if (valid_computer_choices.indexOf(computer_choice) == 2) {
+                win = 1;
+            } else if (valid_computer_choices.indexOf(computer_choice) == 0) {
+                win = 2;
+            }
+            break;
+
+        default:
+            break;
+    }
+
+
+    return win;
+}
+
+function do_player_win() {
+    let result_div = document.getElementById('results');
+    let player_score = document.getElementById('user_score');
+    result_div.innerHTML = "You win!";
+    let current_score = parseInt(player_score.innerText) + 1
+    player_score.innerHTML = current_score
+}
+
+function do_computer_Win() {
+    let result_div = document.getElementById('results');
+    let copmputer_score = document.getElementById('computer_score');
+    result_div.innerHTML = "You lost, better luck next time";
+    let current_score = parseInt(copmputer_score.innerText) + 1
+    copmputer_score.innerHTML = current_score
+}
+
+function obtain_computer_choice() {
+    return valid_computer_choices[Math.floor(Math.random() * valid_computer_choices.length)]
 }
